37626295524565fd8bffbad5175db68f81233c0e
Michael Harvey <mikeytown19@gmail.com>
2019-05-19 17:24:50 -0600

    almost have the desktop boxes done

--- src/components/tabBoxes.js
+++ src/components/tabBoxes.js
@@ -8,21 +8,34 @@ const {colors, maxWidth} = theme;
 const TabContainer = styled(Container)`
   padding: 0;
   display: flex;
-  margin-top: -50px;
+  margin-top: -75px;
   color: ${colors.white};
 
 
 
 `
+const ImageText = styled.div`
+      position: absolute;
+      color: ${colors.white};
+      top: 0;
+      z-index: 20;
+      width: 100%;
+      height: 100%;
+      text-align: center;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+`
 
-const ImageContainer = styled.div`
+const ImageContainer = styled(Link)`
     width: 400px;
     background-color: ${colors.red};
     position: relative;
 
     &:hover {
       &:before {
-        opacity: 0;
+        opacity: .5;
       }
     }
 
@@ -31,7 +44,7 @@ const ImageContainer = styled.div`
       position: absolute;
       height: 100%;
       width: 100%;
-      background-color: ${props => props.noBg ? '' : colors.blue};
+      background-color: ${props => props.nobg ? '' : colors.blue};
       opacity: .8;
       z-index: 10;
       content: '';
@@ -42,22 +55,24 @@ const ImageContainer = styled.div`
 const TabBoxes = () => (
   <Row noPadding>
     <TabContainer>
-      <ImageContainer noBg>
-        <div>Contribute</div>
+      <ImageContainer to='/' nobg="true">
+        <ImageText> <h4>Contribute</h4></ImageText>
       </ImageContainer>
 
-      <ImageContainer>
+      <ImageContainer to='/page-2'>
         <Image src='community.jpg' />
-        <div>Contribute</div>
+        <ImageText> <h4>Volunteer</h4></ImageText>
 
       </ImageContainer>
 
-      <ImageContainer>
+      <ImageContainer to='/page-2'>
         <Image src='email-updates.jpg' />
+        <ImageText> <h4>Email Updates</h4></ImageText>
       </ImageContainer>
 
-      <ImageContainer>
+      <ImageContainer to='/page-2'>
         <Image src='phone.jpg' />
+        <ImageText> <h4>Latest News</h4></ImageText>
       </ImageContainer>
 
     </TabContainer>
