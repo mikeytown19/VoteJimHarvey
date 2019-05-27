c11cd551d1d5eafefc3524a92551fbb0de98775b
Michael Harvey <mikeytown19@gmail.com>
2019-05-27 15:41:25 -0600

    added the modal for the images on the endorsments page

--- src/components/endorseCard.js
+++ src/components/endorseCard.js
@@ -2,7 +2,9 @@ import React from "react"
 import { StaticQuery, graphql } from "gatsby"
 import Img from "gatsby-image"
 import styled from '@emotion/styled'
-
+import Popup from 'reactjs-popup'
+import {theme } from '../styles'
+const {colors} = theme;
 /*
  * This component is built using `gatsby-image` to automatically serve optimized
  * images with lazy loading and reduced file sizes. The image is loaded using a
@@ -14,6 +16,8 @@ import styled from '@emotion/styled'
  * - `StaticQuery`: https://gatsby.dev/staticquery
  */
 
+
+
 const CardContainer = styled.div`
   color: red;
   display: flex;
@@ -21,15 +25,61 @@ const CardContainer = styled.div`
   margin: auto;
   max-width: 1200px;
   justify-content: center;
-  .gatsby-image-wrapper {
+
+  .img_container {
     height: auto;
     width: 100%;
     max-width: 400px;
     margin: 10px;
     cursor: pointer;
+    position: relative;
+
+
+    .button {
+      position: absolute;
+      height: 100%;
+      width: 100%;
+      z-index: 100;
+    }
+
+    .close {
+      cursor: pointer;
+      font-size: 30px;
+    }
 
   }
 `
+
+const PopupStyles = styled(Popup)`
+color: ${colors.blue};
+margin: 0;
+max-width: 100% !important;
+.popup-content {
+  width: 75% !important;
+}
+`
+
+const PopupExample =  ({img}) => {
+  return (
+    <PopupStyles  position="center center"trigger={<span className="button"></span>} modal>
+    {close => (
+      <div className="modal">
+        <a className="close" onClick={close}>
+          &times;
+        </a>
+
+
+     <Img  fluid={img}/>
+
+
+      </div>
+    )}
+  </PopupStyles>
+
+)
+}
+
+
 
 const EndorsmentCard = ({data}) => {
 
@@ -42,10 +92,14 @@ const EndorsmentCard = ({data}) => {
   })
   return (
     <CardContainer>
-      {console.log(endorseImages)}
+
       {endorseImages.map((item, index) => {
+        let image = item.node.childImageSharp.fluid;
         return (
-          <Img key={index} fluid={item.node.childImageSharp.fluid}/>
+          <div key={index} className="img_container">
+            <PopupExample img={image} />
+            <Img  fluid={image}/>
+          </div>
         )
       })}
 
@@ -65,7 +119,7 @@ const EndorsmentCard = ({data}) => {
         node {
           relativePath
           childImageSharp {
-            fluid(maxWidth: 800) {
+            fluid(maxWidth: 1200) {
               ...GatsbyImageSharpFluid
               originalName
             }
--- src/pages/endorsements.js
+++ src/pages/endorsements.js
@@ -2,12 +2,28 @@
 
 import React from 'react'
 import Layout from "../components/layout"
+import styled from '@emotion/styled'
 import SEO from "../components/seo"
 import EndorseCard from '../components/endorseCard'
 import {theme, Container, Row } from '../styles'
+import Jim from '../images/Logos.2.svg'
+const {colors} = theme;
 
+const HeroImage = styled.img`
+  max-height: 400px;
+  height: auto;
+  width: 100%;
+  margin-bottom: 70px;
 
+`
 
+const HeroContainer = styled(Container)`
+  margin: auto;
+  padding-top: 30px;
+  text-align: center;
+`
+const H1 = styled.h1`
+color: ${colors.blue};`
 const Endorsments = () => {
 
 
@@ -15,9 +31,17 @@ const Endorsments = () => {
 
     <Layout>
     <SEO title="Endorsments" />
+
+    <Row noPadding bgColor={colors.blue}>
+      <HeroContainer>
+        <HeroImage src={Jim} alt=''/>
+      </HeroContainer>
+    </Row>
+
     <Row>
       <Container>
-        <h1>Endorsments</h1>
+
+        <H1>Endorsments</H1>
 
       <EndorseCard />
       </Container>
